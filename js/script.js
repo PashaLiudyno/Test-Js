12a572eeddccb045a514673f5a1c3ebe67851c58
Pashameister <hvgbh47@gmail.com>
2023-12-11 14:12:14 +0300

    movies comments

--- js/script.js
+++ js/script.js
@@ -35,14 +35,33 @@ console.log(answer);
 // console.log(`https://someurl.com/${category}/5`);
 // const user = 'Иван';
 // alert(`Привет, ${user}`);
-let incr = 10,
-    decr = 10;
-++incr; //когда ставим операторы '++ или --' Перед значением это форма записи - Префиксная. когда после - Постиксная
---decr; // ++ это плюс 1, -- это минус 1
-console.log(incr); //можно ставить ++ или -- сразу в выводе, console.log(++incr);
-console.log(decr);
-console.log(2*4===8);//если === то это строгое сравнение, например по типу данных, число равняется строго числу
+//let incr = 10,
+//    decr = 10;
+//++incr; //когда ставим операторы '++ или --' Перед значением это форма записи - Префиксная. когда после - Постиксная
+//--decr; // ++ это плюс 1, -- это минус 1
+//console.log(incr); //можно ставить ++ или -- сразу в выводе, console.log(++incr);
+//console.log(decr);
+//console.log(2*4===8);//если === то это строгое сравнение, например по типу данных, число равняется строго числу
 //  && - 'И' ; || - 'ИЛИ' ; != - 'не равно'
-const isChecked = true,
-      isClose = true;
- console.log(isChecked || !isClose);  // ! - оператор отрицания (меняет true на false и наоборот)   
+//const isChecked = true,
+//      isClose = true;
+// console.log(isChecked || !isClose);  // ! - оператор отрицания (меняет true на false и наоборот)   
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); //prompt - вопрос, + это из строчного в числовую форму записи
+
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
+
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
